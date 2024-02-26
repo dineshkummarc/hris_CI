1c5065cf0cf687195ca239b4b6b6221088a22442
Paulus Christofel S <75057796+pythpal20@users.noreply.github.com>
2024-02-27 01:13:31 +0700

    update controller

--- application/controllers/Interview.php
+++ application/controllers/Interview.php
@@ -23,4 +23,110 @@ class Interview extends CI_Controller
         $this->load->view('interview/hasilRekap', $data);
         $this->load->view('templates/footer');
     }
+
+    private function _namaHrd($idhrd)
+    {
+        $row    = $this->db->get_where('tb_user', ['user_id' => $idhrd])->row_array();
+
+        return $row['TXT_NAMA'];
+    }
+
+    private function _urutanIDuser()
+    {
+        $this->load->model("m_karyawan");
+        $durt = $this->m_karyawan->getUrutanPelamar();
+
+        foreach ($durt->result() as $row) {
+            $urutan = $row->idArr;
+            $urutan = (int)$urutan + 1;
+            $user_id = "PL" . date('ymd') . "-" . sprintf("%04s", $urutan);
+        }
+
+        return $user_id;
+    }
+
+    function datarhi()
+    {
+        $no = 1;
+        $this->db->order_by('tgl_interview', 'DESC');
+        $arrdat     = $this->db->get('tb_pelamar');
+        $data       = array();
+
+        foreach ($arrdat->result() as $row) {
+            if ($row->hasil_intervie == '0') {
+                $hasil = "<span class='label label-danger'>Ditolak</span>";
+            } elseif ($row->hasil_intervie == '1') {
+                $hasil = "<span class='label label-success'>Diterima</span>";
+            } elseif ($row->hasil_intervie == '2') {
+                $hasil = "<span class='label label-warning'>Dipertimbangkan</span>";
+            }
+            $tahun_p_1 = "";
+            $tahun_p_2 = "";
+            $tahun_p_3 = "";
+            $tahun_p_4 = "";
+            $tahun_p_5 = "";
+            $tahun_p_6 = "";
+
+            if (strpos($row->pengalaman_kerja1, "|") !== false) {
+                $tahun_p_1  = explode("|", $row->pengalaman_kerja1)[0];
+                $posisi_p_1 = explode("|", $row->pengalaman_kerja1)[1];
+                $bidang_p_1 = explode("|", $row->pengalaman_kerja1)[2];
+            }
+
+            if (strpos($row->pengalaman_kerja2, "|") !== false) {
+                $tahun_p_2 = explode("|", $row->pengalaman_kerja2)[0];
+                $posisi_p_2 = explode("|", $row->pengalaman_kerja2)[1];
+                $bidang_p_2 = explode("|", $row->pengalaman_kerja2)[2];
+            }
+            if (strpos($row->pengalaman_kerja3, "|") !== false) {
+                $tahun_p_3 = explode("|", $row->pengalaman_kerja3)[0];
+                $posisi_p_3 = explode("|", $row->pengalaman_kerja3)[1];
+                $bidang_p_3 = explode("|", $row->pengalaman_kerja3)[2];
+            }
+            if (strpos($row->pengalaman_kerja4, "|") !== false) {
+                $tahun_p_4 = explode("|", $row->pengalaman_kerja4)[0];
+                $posisi_p_4 = explode("|", $row->pengalaman_kerja4)[1];
+                $bidang_p_4 = explode("|", $row->pengalaman_kerja4)[2];
+            }
+            if (strpos($row->pengalaman_kerja5, "|") !== false) {
+                $tahun_p_5 = explode("|", $row->pengalaman_kerja5)[0];
+                $posisi_p_5 = explode("|", $row->pengalaman_kerja5)[1];
+                $bidang_p_5 = explode("|", $row->pengalaman_kerja5)[2];
+            }
+            if (strpos($row->pengalaman_kerja6, "|") !== false) {
+                $tahun_p_6 = explode("|", $row->pengalaman_kerja6)[0];
+                $posisi_p_6 = explode("|", $row->pengalaman_kerja6)[1];
+                $bidang_p_6 = explode("|", $row->pengalaman_kerja6)[2];
+            }
+
+            $ke_vaksin = explode("|", $row->vaksin)[0];
+            $nama_vaksin = explode("|", $row->vaksin)[1];
+            $data[] = array(
+                'no'    => $no++,
+                'id'    => $row->id_pelamar,
+                'nama'  => $row->nama_pelamar,
+                'tglinterview'  => $row->tgl_interview,
+                'posisi'        => $row->posisi_yangdilamar,
+                'usia'          => $row->usia_pelamar,
+                'pendidikan'    => $row->pendidikan_terahir,
+                'fakultas'      => $row->fakultas_pelamar,
+                'jurusan'       => $row->jurusan_pelamar,
+                'namaskolah'    => $row->nama_sekolah,
+                'PengalamanKerja_1'   => ($tahun_p_1 == "") ? "" : $tahun_p_1 . " Tahun, " . $posisi_p_1 . ", " . $bidang_p_1,
+                'PengalamanKerja_2'   => ($tahun_p_2 == "") ? "" : $tahun_p_2 . " Tahun, " . $posisi_p_2 . ", " . $bidang_p_2,
+                'PengalamanKerja_3'   => ($tahun_p_3 == "") ? "" : $tahun_p_3 . " Tahun, " . $posisi_p_3 . ", " . $bidang_p_3,
+                'PengalamanKerja_4'   => ($tahun_p_4 == "") ? "" : $tahun_p_4 . " Tahun, " . $posisi_p_4 . ", " . $bidang_p_4,
+                'PengalamanKerja_5'   => ($tahun_p_5 == "") ? "" : $tahun_p_5 . " Tahun, " . $posisi_p_5 . ", " . $bidang_p_5,
+                'PengalamanKerja_6'   => ($tahun_p_6 == "") ? "" : $tahun_p_6 . " Tahun, " . $posisi_p_6 . ", " . $bidang_p_6,
+                'kekurangan'    => $row->kekurangan_pelamar,
+                'kelebihan'     => $row->kelebihan_pelamar,
+                'vaksin'        => $ke_vaksin . ", vaksin: " . $nama_vaksin,
+                'hasil'         => $hasil,
+                'alasan'        => $row->alasan_hasil,
+                'HRD'       => $this->_namaHrd($row->input_by)
+            );
+        }
+        header('Content-Type: application/json');
+        echo json_encode($data);
+    }
 }
