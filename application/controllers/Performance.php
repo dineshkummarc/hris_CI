3d587acd3fdab70720a4263d6e662c202f4e4026
Paulus Christofel S <75057796+pythpal20@users.noreply.github.com>
2024-02-29 11:49:49 +0700

    Modul IPR start to work

--- application/controllers/Performance.php
+++ application/controllers/Performance.php
@@ -8,4 +8,17 @@ class Performance extends CI_Controller
         parent::__construct();
         is_logged_in();
     }
+
+    public function ipr()
+    {
+        $data['title']  = "IPR";
+        $data['user']   = $this->db->get_where('tb_user', ['TXT_EMAIL' => $this->session->userdata('email')])->row_array();
+        $data['rar']    = $this->db->get_where('role_access_rights', ['id' => $this->session->userdata('rar_id')])->row_array();
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/headbar', $data);
+        $this->load->view('performance/ipr', $data);
+        $this->load->view('templates/footer');
+    }
 }
